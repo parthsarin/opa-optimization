6961e005412434a697027eabf2663fc570b0512d
Parth Sarin <psarin@stanford.edu>
2022-06-01 16:55:18 -0700

    initial commit

---
+++ compute_sims.py
@@ -0,0 +1,283 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from bisect import bisect
+import matplotlib.pyplot as plt
+from functools import cache
+
+##################
+# DO NOTHING
+##################
+
+# print('SCENARIO: DO NOTHING')
+# downpayment = 0.20     # percent of price
+# interest = 0.055       # percent of price
+# mortgage_duration = 30  # years
+# rofr = 0               # percent reduction in sales price
+# spillover_amt = 0      # percent of sales that are affected by spillover
+# spillover_mag = 0      # reduction of sale price due to spillover
+# off_market = 0.25      # percent of sales off markett
+# dti = 0.36             # debt-to-income ratio that's required to get the mortgage approved
+# ami = 149_600          # average median income
+# sfh = 1                # include single family homes in OPA? 1 = yes, 0 = no
+
+
+##################
+# FULL OPA
+##################
+
+print('SCENARIO: FULL OPA')
+downpayment = 0.20     # percent of price
+interest = 0.055       # percent of price
+mortgage_duration = 30 # years
+rofr = 0.1             # percent reduction in sales price
+spillover_amt = 0.4    # percent of sales that are affected by spillover
+spillover_mag = 0.1    # reduction of sale price due to spillover
+off_market = 0.1       # percent of sales off markett
+dti = 0.36             # debt-to-income ratio that's required to get the mortgage approved
+ami = 149_600          # average median income
+sfh = 1                # include single family homes in OPA? 1 = yes, 0 = no
+
+
+##################
+# OPA wo ROFR
+##################
+
+# print('SCENARIO: OPA WITHOUT ROFR')
+# downpayment = 0.20     # percent of price
+# interest = 0.055       # percent of price
+# mortgage_duration = 30 # years
+# rofr = 0               # percent reduction in sales price
+# spillover_amt = 0.2    # percent of sales that are affected by spillover
+# spillover_mag = 0.05   # reduction of sale price due to spillover
+# off_market = 0.1       # percent of sales off markett
+# dti = 0.36             # debt-to-income ratio that's required to get the mortgage approved
+# ami = 149_600          # average median income
+# sfh = 1                # include single family homes in OPA? 1 = yes, 0 = no
+
+##################
+# OPA WITH MODIFIED ELIGIBILITY
+##################
+
+# print('SCENARIO: OPA WITH MODIFIED ELIGIBILITY')
+# downpayment = 0.20     # percent of price
+# interest = 0.055       # percent of price
+# mortgage_duration = 30 # years
+# rofr = 0.1             # percent reduction in sales price
+# spillover_amt = 0.05   # percent of sales that are affected by spillover
+# spillover_mag = 0.1    # reduction of sale price due to spillover
+# off_market = 0.1       # percent of sales off markett
+# dti = 0.36             # debt-to-income ratio that's required to get the mortgage approved
+# ami = 149_600          # average median income
+# sfh = 0                # include single family homes in OPA? 1 = yes, 0 = no
+
+
+funding = 2_000_000
+df = pd.read_csv('final_results_1000.csv')
+df.columns = [c.lower().replace(' ', '_') for c in df.columns]
+
+n = len(df)
+sim_ids = set(df.simulation)
+num_sims = len(sim_ids)
+num_per_sim = n / num_sims
+
+
+##################
+# COMPUTATION
+##################
+def get_category_coarse(x):
+    idx = bisect([80, 160], x)
+    cats = ['Low Income', 'Middle Income', 'Upper Income']
+    return cats[idx]
+
+df['adj_price'] = df.price
+df['adj_price'] = df.adj_price - (np.random.random(n) < spillover_amt) * spillover_mag * df.price
+
+df['p_ami'] = df.income / ami * 100
+df['mortgage'] = (1 - downpayment) * ((1 + interest) ** mortgage_duration) * (1 - rofr) * df.adj_price / mortgage_duration
+df['viable'] = (df.mortgage / df.income <= dti) 
+
+# 0 = not eligible, 1 = still eligible
+df['eligible_market'] = 1 - (np.random.random(n) < off_market)
+df['eligible_sfh'] = np.ones(n) if sfh == 1 else (df.property_indicator != 10) * 1
+
+df['viable_eligible'] = df.eligible_market * df.eligible_sfh * df.viable
+df['income_category'] = df.p_ami.apply(get_category_coarse)
+
+
+##################
+# OPTIMIZATION
+##################
+def count_sum_to_thresh(df, thresh):
+    a = df.gap
+    for i in range(len(a)):
+        if sum(a[:i]) > thresh:
+            break
+    num_helped = i - 1
+
+    return num_helped, df.iloc[:num_helped]
+
+def count_sum_to_thresh_thanos(a, thresh, full_df):
+    for i in range(len(a)):
+        if sum(a[:i]) > thresh:
+            break
+    num_helped = i - 1
+
+    return num_helped, full_df.iloc[:num_helped]
+
+def surgical(funding):
+    potential = df[df.eligible_market * df.eligible_sfh == 1]
+    potential = potential[potential.viable_eligible == 0]
+    potential['gap'] = potential.mortgage / dti - potential.income
+    
+    num_helped = []
+    people_helped = []
+
+    for sid in sim_ids:
+        x = potential[potential.simulation == sid]
+        x = x.sort_values('gap')
+        nh, ph = count_sum_to_thresh(x, funding)
+
+        num_helped.append(nh)
+        people_helped.append(ph)
+
+    num_helped = np.array(num_helped)
+    people_helped = pd.concat(people_helped)
+
+    return num_helped, people_helped
+
+
+def surgical_equitable(funding):
+    potential = df[df.eligible_market * df.eligible_sfh == 1]
+    potential = potential[potential.viable_eligible == 0]
+    potential['gap'] = potential.mortgage / dti - potential.income
+    
+    num_helped = []
+    people_helped = []
+
+    for sid in sim_ids:
+        x = potential[potential.simulation == sid]
+        x = x[x.p_ami < 80]
+        x = x.sort_values('gap')
+
+        nh, ph = count_sum_to_thresh(x, funding)
+
+        num_helped.append(nh)
+        people_helped.append(ph)
+
+    num_helped = np.array(num_helped)
+    people_helped = pd.concat(people_helped)
+
+    return num_helped, people_helped
+
+
+def thanos(funding):
+    potential = df[df.eligible_market * df.eligible_sfh == 1]
+    potential = potential[potential.viable_eligible == 0]
+    potential['gap'] = potential.mortgage / dti - potential.income
+
+    num_helped = []
+    people_helped = []
+
+    for sid in sim_ids:
+        x = potential[potential.simulation == sid]
+        x = x.sample(frac=0.5)  # thanos
+        x = x.sort_values('gap')
+        a = x.gap
+        a = a / 0.63  # https://web.archive.org/web/20200321115234id_/https://www.innovations.harvard.edu/sites/default/files/hpd_0202_stegman.pdf
+
+        nh, ph = count_sum_to_thresh_thanos(a, funding, x)
+
+        num_helped.append(nh)
+        people_helped.append(ph)
+
+    num_helped = np.array(num_helped)
+    people_helped = pd.concat(people_helped)
+
+    return num_helped, people_helped
+
+
+def thanos_equitable(funding):
+    potential = df[df.eligible_market * df.eligible_sfh == 1]
+    potential = potential[potential.viable_eligible == 0]
+    potential['gap'] = potential.mortgage / dti - potential.income
+
+    num_helped = []
+    people_helped = []
+
+    for sid in sim_ids:
+        x = potential[potential.simulation == sid]
+        x = x[x.p_ami < 80]
+        x = x.sample(frac=0.5)  # thanos
+        x = x.sort_values('gap')
+        a = x.gap
+        a = a / 0.63  # https://web.archive.org/web/20200321115234id_/https://www.innovations.harvard.edu/sites/default/files/hpd_0202_stegman.pdf
+
+        nh, ph = count_sum_to_thresh_thanos(a, funding, x)
+
+        num_helped.append(nh)
+        people_helped.append(ph)
+
+    num_helped = np.array(num_helped)
+    people_helped = pd.concat(people_helped)
+
+    return num_helped, people_helped
+
+
+##################
+# RESULTS
+##################
+nh, ph = thanos(funding)
+nhe, phe = thanos_equitable(funding)
+
+def perc_low_income(people_helped, full_df):
+    try:
+        num_low_income_helped = people_helped.income_category.value_counts()['Low Income']
+    except KeyError:
+        num_low_income_helped = 0
+    
+    # num_low_income_sfh = sum(
+    #     full_df.eligible_market 
+    #     * (1 - full_df.eligible_sfh)
+    #     * full_df.viable 
+    #     * (full_df.income_category == 'Low Income')
+    # ) / num_sims
+
+    num_low_income_afford = sum((full_df.viable_eligible == 1) & (full_df.income_category == 'Low Income'))
+    total_low_income = sum(full_df.income_category == 'Low Income')
+    return (num_low_income_afford + num_low_income_helped) / total_low_income # could add num_low_income_sfh
+
+
+def perc_poc(people_helped, full_df):
+    try:
+        num_poc_helped = len(people_helped) - people_helped.race.value_counts()['White']
+    except KeyError:
+        num_poc_helped = 0
+    
+    # num_poc_sfh = sum(
+    #     full_df.eligible_market
+    #     * (1 - full_df.eligible_sfh)
+    #     * full_df.viable
+    #     * (full_df.race != 'White')
+    # )
+    num_poc_afford = sum((full_df.viable_eligible == 1) &
+                         (full_df.race != 'White'))
+    total_poc = sum(full_df.race != 'White')
+    return (num_poc_afford + num_poc_helped) / total_poc # could add num_poc_sfh
+
+
+# sfh = (df.eligible_market * (1 - df.eligible_sfh) * df.viable).sum() / num_sims
+no_funding = df.groupby('simulation').sum().viable_eligible
+p_ev = (no_funding + nh).mean() / num_per_sim # could add sfh
+
+p_li_realistic = perc_low_income(ph, df)
+p_li_realistic_equitable = perc_low_income(phe, df)
+
+p_poc_realistic = perc_poc(ph, df)
+p_poc_realistic_equitable = perc_poc(phe, df)
+
+print(f'Eligibility & Viability: {p_ev:.2%}')
+print(f'Income Equity (Realistic): {p_li_realistic:.2%}')
+print(f'Income Equity (Realistic Equitable): {p_li_realistic_equitable:.2%}')
+print(f'Racial Equity (Realistic): {p_poc_realistic:.2%}')
+print(f'Racial Equity (Realistic Equitable): {p_poc_realistic_equitable:.2%}')
---
+++ compute_spillover.py
@@ -0,0 +1,178 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from bisect import bisect
+import matplotlib.pyplot as plt
+from functools import cache
+
+##################
+# FULL OPA
+##################
+
+# print('SCENARIO: FULL OPA')
+# downpayment = 0.20     # percent of price
+# interest = 0.055       # percent of price
+# mortgage_duration = 30  # years
+# rofr = 0.1             # percent reduction in sales price
+# spillover_mag = 0.1    # reduction of sale price due to spillover
+# off_market = 0.1       # percent of sales off markett
+# dti = 0.36             # debt-to-income ratio that's required to get the mortgage approved
+# ami = 149_600          # average median income
+# sfh = 1                # include single family homes in OPA? 1 = yes, 0 = no
+
+
+##################
+# OPA wo ROFR
+##################
+
+# print('SCENARIO: OPA WITHOUT ROFR')
+# downpayment = 0.20     # percent of price
+# interest = 0.055       # percent of price
+# mortgage_duration = 30 # years
+# rofr = 0               # percent reduction in sales price
+# spillover_mag = 0.05   # reduction of sale price due to spillover
+# off_market = 0.1       # percent of sales off markett
+# dti = 0.36             # debt-to-income ratio that's required to get the mortgage approved
+# ami = 149_600          # average median income
+# sfh = 1                # include single family homes in OPA? 1 = yes, 0 = no
+
+##################
+# OPA WITH MODIFIED ELIGIBILITY
+##################
+
+print('SCENARIO: OPA WITH MODIFIED ELIGIBILITY')
+downpayment = 0.20     # percent of price
+interest = 0.055       # percent of price
+mortgage_duration = 30 # years
+rofr = 0.1             # percent reduction in sales price
+spillover_mag = 0.1    # reduction of sale price due to spillover
+off_market = 0.1       # percent of sales off markett
+dti = 0.36             # debt-to-income ratio that's required to get the mortgage approved
+ami = 149_600          # average median income
+sfh = 0                # include single family homes in OPA? 1 = yes, 0 = no
+
+
+rawdog = pd.read_csv('final_results_1000.csv')
+rawdog.columns = [c.lower().replace(' ', '_') for c in rawdog.columns]
+
+n = len(rawdog)
+sim_ids = set(rawdog.simulation)
+num_sims = len(sim_ids)
+num_per_sim = n / num_sims
+
+
+##################
+# COMPUTATION
+##################
+def get_category_coarse(x):
+    idx = bisect([80, 160], x)
+    cats = ['Low Income', 'Middle Income', 'Upper Income']
+    return cats[idx]
+
+def get_df(spillover_amt):
+    df = rawdog.copy()
+
+    df['adj_price'] = df.price
+    df['adj_price'] = df.adj_price - (np.random.random(n) < spillover_amt) * spillover_mag * df.price
+
+    df['p_ami'] = df.income / ami * 100
+    df['mortgage'] = (1 - downpayment) * ((1 + interest) ** mortgage_duration) * (1 - rofr) * df.adj_price / mortgage_duration
+    df['viable'] = (df.mortgage / df.income <= dti) 
+
+    # 0 = not eligible, 1 = still eligible
+    df['eligible_market'] = 1 - (np.random.random(n) < off_market)
+    df['eligible_sfh'] = np.ones(n) if sfh == 1 else (df.property_indicator != 10) * 1
+
+    df['viable_eligible'] = df.eligible_market * df.eligible_sfh * df.viable
+    df['income_category'] = df.p_ami.apply(get_category_coarse)
+
+    return df
+
+
+##################
+# OPTIMIZATION
+##################
+def count_sum_to_thresh_thanos(a, thresh, full_df):
+    for i in range(len(a)):
+        if sum(a[:i]) > thresh:
+            break
+    num_helped = i - 1
+
+    return num_helped, full_df.iloc[:num_helped]
+
+
+def thanos(funding, df):
+    potential = df[df.eligible_market * df.eligible_sfh == 1]
+    potential = potential[potential.viable_eligible == 0]
+    potential['gap'] = potential.mortgage / dti - potential.income
+
+    num_helped = []
+    people_helped = []
+
+    for sid in sim_ids:
+        x = potential[potential.simulation == sid]
+        x = x.sample(frac=0.5)  # thanos
+        x = x.sort_values('gap')
+        a = x.gap
+        a = a / 0.63  # https://web.archive.org/web/20200321115234id_/https://www.innovations.harvard.edu/sites/default/files/hpd_0202_stegman.pdf
+
+        nh, ph = count_sum_to_thresh_thanos(a, funding, x)
+
+        num_helped.append(nh)
+        people_helped.append(ph)
+
+    num_helped = np.array(num_helped)
+    people_helped = pd.concat(people_helped)
+
+    return num_helped, people_helped
+
+
+##################
+# RESULTS
+##################
+def perc_low_income(people_helped, full_df):
+    try:
+        num_low_income_helped = people_helped.income_category.value_counts()['Low Income']
+    except KeyError:
+        num_low_income_helped = 0
+        
+    num_low_income_afford = sum((full_df.viable_eligible == 1) & (full_df.income_category == 'Low Income'))
+    total_low_income = sum(full_df.income_category == 'Low Income')
+    return (num_low_income_afford + num_low_income_helped) / total_low_income
+
+
+def perc_poc(people_helped, full_df):
+    try:
+        num_poc_helped = len(people_helped) - people_helped.race.value_counts()['White']
+    except KeyError:
+        num_poc_helped = 0
+    
+    num_poc_afford = sum((full_df.viable_eligible == 1) &
+                         (full_df.race != 'White'))
+    total_poc = sum(full_df.race != 'White')
+    return (num_poc_afford + num_poc_helped) / total_poc
+
+funding = 2_000_000
+for spillover_amt in range(5, 101):
+    print(f'progress: {spillover_amt} / 100')
+    spillover_amt = spillover_amt / 100
+
+    df = get_df(spillover_amt)
+    nh, ph = thanos(funding, df)
+
+    helped_overall = pd.concat([
+        # df[df.eligible_market * df.viable == 1],
+        df[df.viable_eligible == 1],
+        ph
+    ])
+    helped_overall['spilled_over'] = helped_overall['price'] != helped_overall['adj_price']
+    spillover = sum(
+        helped_overall['spilled_over'] * (helped_overall['price'] - helped_overall['adj_price'])
+    ) / num_sims
+    print(f'{spillover = }')
+
+    if spillover >= funding:
+        break
+
+
+print(f'Spillover (Breakeven): {spillover_amt:.0%}')
